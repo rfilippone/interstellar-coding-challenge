227f3916e41821f70782d2065aac21120f2b445d
rfilippone <rfilippone@gmail.com>
2019-02-11 19:37:56 +0100

    The service only supports POST
    Protect the beaviour with an end to end test

---
+++ src/main/java/com/interstallar/codingchallenge/infrastructure/GenerateImagesController.java
@@ -0,0 +1,14 @@
+package com.interstallar.codingchallenge.infrastructure;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GenerateImagesController {
+
+  @RequestMapping(method = RequestMethod.POST, path = "/generate-images")
+  public String name() {
+    return "";
+  }
+}
--- src/test/java/com/interstallar/codingchallenge/CodingChallengeApplicationTests.java
+++ src/test/java/com/interstallar/codingchallenge/CodingChallengeApplicationTests.java
@@ -5,8 +5,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -20,4 +23,9 @@ public class CodingChallengeApplicationTests {
     this.webClient.get().uri("/").exchange().expectStatus().isNotFound();
   }
 
+  @Test
+  public void returns404OnWrongMethod() {
+    this.webClient.get().uri("/generate-images").exchange().expectStatus().isEqualTo(HttpStatus.METHOD_NOT_ALLOWED)
+        .expectHeader().valueEquals(HttpHeaders.ALLOW, RequestMethod.POST.name());
+  }
 }
