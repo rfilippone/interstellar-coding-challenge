986c7fbcd70b2d490c2ef157e130f99c451b14d5
rfilippone <rfilippone@gmail.com>
2019-02-11 19:06:50 +0100

    End to end test for root route
    The root route must return a 404

--- src/test/java/com/interstallar/codingchallenge/CodingChallengeApplicationTests.java
+++ src/test/java/com/interstallar/codingchallenge/CodingChallengeApplicationTests.java
@@ -2,16 +2,22 @@ package com.interstallar.codingchallenge;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class CodingChallengeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Autowired
+  private WebTestClient webClient;
+
+  @Test
+  public void returns404OnWrongURI() {
+    this.webClient.get().uri("/").exchange().expectStatus().isNotFound();
+  }
 
 }
-
