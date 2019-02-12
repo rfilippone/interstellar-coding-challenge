e602584a1ef5da3b24a090130f8cde522d72e851
rfilippone <rfilippone@gmail.com>
2019-02-12 20:26:07 +0100

    First implemenattion of a Granule instantiation

--- src/main/java/com/interstellar/codingchallenge/domain/Granule.java
+++ src/main/java/com/interstellar/codingchallenge/domain/Granule.java
@@ -1,5 +1,37 @@
 package com.interstellar.codingchallenge.domain;
 
-public class Granule {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
+public class Granule {
+  // Pattern: T33UUP_20180804T100031_B01.tif
+  public static enum Bands {
+    B02, B03, B04, B05, B06, B07, B09
+  }
+
+  private Map<Bands, String> map;
+
+  private Granule() {
+    map = new HashMap<Granule.Bands, String>();
+  }
+
+  public static Granule fromfiles(List<String> files) {
+    Granule granule = new Granule();
+    Pattern p = Pattern.compile("");
+    for (String file : files) {
+      String bandString = file.substring(23, 26);
+      try {
+        granule.map.put(Bands.valueOf(bandString), file);
+      } catch (Exception e) {
+      }
+    }
+
+    return granule;
+  }
+
+  public boolean has(Bands band) {
+    return map.containsKey(band);
+  }
 }
---
+++ src/test/java/com/interstellar/codingchallenge/domain/GranuleTest.java
@@ -0,0 +1,38 @@
+package com.interstellar.codingchallenge.domain;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class GranuleTest {
+
+  @Test
+  public void providesNoBandsWhenNoFiles() {
+    Granule granule = Granule.fromfiles(new ArrayList<>());
+    assertFalse(granule.has(Granule.Bands.B02));
+    assertFalse(granule.has(Granule.Bands.B03));
+    assertFalse(granule.has(Granule.Bands.B04));
+    assertFalse(granule.has(Granule.Bands.B05));
+    assertFalse(granule.has(Granule.Bands.B06));
+    assertFalse(granule.has(Granule.Bands.B07));
+    assertFalse(granule.has(Granule.Bands.B09));
+  }
+
+  @Test
+  public void providesB02IfPresent() {
+    Granule granule = Granule
+        .fromfiles(Arrays.asList("T33UUP_20180804T100031_B01.tif", "T33UUP_20180804T100031_B02.tif"));
+    assertTrue(granule.has(Granule.Bands.B02));
+    assertFalse(granule.has(Granule.Bands.B03));
+    assertFalse(granule.has(Granule.Bands.B04));
+    assertFalse(granule.has(Granule.Bands.B05));
+    assertFalse(granule.has(Granule.Bands.B06));
+    assertFalse(granule.has(Granule.Bands.B07));
+    assertFalse(granule.has(Granule.Bands.B09));
+  }
+
+}
