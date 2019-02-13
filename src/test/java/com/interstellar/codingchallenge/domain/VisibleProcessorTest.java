33401a3641c18b67ecd23f1328e81e8321518e23
rfilippone <rfilippone@gmail.com>
2019-02-13 18:54:16 +0100

    More test to cover visible processor input validity

--- src/test/java/com/interstellar/codingchallenge/domain/VisibleProcessorTest.java
+++ src/test/java/com/interstellar/codingchallenge/domain/VisibleProcessorTest.java
@@ -1,6 +1,8 @@
 package com.interstellar.codingchallenge.domain;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.Test;
 
@@ -15,4 +17,27 @@ public class VisibleProcessorTest {
     processor.process(granule);
   }
 
+  @Test(expected = NotEnoughImages.class)
+  public void cannotProcessMissingB02Granule() throws NotEnoughImages {
+    Granule granule = Granule.fromfiles(
+        Arrays.asList(new File("T33UUP_20180804T100031_B03.tif"), new File("T33UUP_20180804T100031_B04.tif")));
+    VisibleProcessor processor = new VisibleProcessor();
+    processor.process(granule);
+  }
+
+  @Test(expected = NotEnoughImages.class)
+  public void cannotProcessMissingB03Granule() throws NotEnoughImages {
+    Granule granule = Granule.fromfiles(
+        Arrays.asList(new File("T33UUP_20180804T100031_B02.tif"), new File("T33UUP_20180804T100031_B04.tif")));
+    VisibleProcessor processor = new VisibleProcessor();
+    processor.process(granule);
+  }
+
+  @Test(expected = NotEnoughImages.class)
+  public void cannotProcessMissingB04Granule() throws NotEnoughImages {
+    Granule granule = Granule.fromfiles(
+        Arrays.asList(new File("T33UUP_20180804T100031_B02.tif"), new File("T33UUP_20180804T100031_B03.tif")));
+    VisibleProcessor processor = new VisibleProcessor();
+    processor.process(granule);
+  }
 }
