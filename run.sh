c16b6fd7a90bb022cc0e9f5322e935f48b672771
rfilippone <rfilippone@gmail.com>
2019-02-14 00:43:59 +0100

    Use assemble target instead of build during start

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./gradlew build || exit 1
+./gradlew assemble || exit 1
 docker build -t java_gdal -f Docker/Dockerfile Docker/ || exit 2
 docker run -v `pwd`/data:/data -v `pwd`/build/libs:/libs -p 8080:8080 java_gdal java -jar libs/coding-challenge-0.0.1-SNAPSHOT.jar || exit 3
 
