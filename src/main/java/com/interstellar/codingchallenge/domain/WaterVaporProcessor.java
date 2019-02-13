6099ee6083acacf0b80da53e0fbb287f0edd2fca
rfilippone <rfilippone@gmail.com>
2019-02-13 23:33:28 +0100

    Implemented waterVapor processor

--- src/main/java/com/interstellar/codingchallenge/domain/WaterVaporProcessor.java
+++ src/main/java/com/interstellar/codingchallenge/domain/WaterVaporProcessor.java
@@ -1,5 +1,9 @@
 package com.interstellar.codingchallenge.domain;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 import org.springframework.stereotype.Service;
 
 import com.interstellar.codingchallenge.domain.Granule.Bands;
@@ -9,11 +13,29 @@ import com.interstellar.codingchallenge.domain.exception.NotEnoughImages;
 public class WaterVaporProcessor implements ImageProcessor {
 
   @Override
-  public byte[] process(Granule granule) throws NotEnoughImages {
+  public byte[] process(Granule granule) throws NotEnoughImages, IOException {
     if (!granule.has(Bands.B09)) {
       throw new NotEnoughImages();
     }
-    // TODO Auto-generated method stub
-    return null;
+    File jpg = File.createTempFile("gdal", ".jpg");
+
+    int result = 0;
+    String cmd1 = "gdal_translate -b 1 -b 1 -b 1 -scale_1 0 100000 0 0  -scale_2 1 100000 0 0 -scale_3 0 16000 0 255 -exponent_3 0.3 -ot Byte -of jpeg "
+        + granule.get(Bands.B09) + " " + jpg.getAbsolutePath();
+    try {
+      Runtime runtime = Runtime.getRuntime();
+      result = runtime.exec(cmd1).waitFor();
+    } catch (InterruptedException | IOException e) {
+      result = 0;
+    }
+    if (result != 0) {
+      throw new NotEnoughImages();
+    }
+
+    byte[] res = Files.readAllBytes(jpg.toPath());
+
+    jpg.delete();
+
+    return res;
   }
 }
--- src/test/java/com/interstellar/codingchallenge/domain/WaterVaporProcessorTest.java
+++ src/test/java/com/interstellar/codingchallenge/domain/WaterVaporProcessorTest.java
@@ -1,6 +1,7 @@
 package com.interstellar.codingchallenge.domain;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -11,14 +12,14 @@ import com.interstellar.codingchallenge.domain.exception.NotEnoughImages;
 public class WaterVaporProcessorTest {
 
   @Test(expected = NotEnoughImages.class)
-  public void cannotProcessEmptyGranule() throws NotEnoughImages {
+  public void cannotProcessEmptyGranule() throws NotEnoughImages, IOException {
     Granule granule = Granule.fromfiles(new ArrayList<>());
     WaterVaporProcessor processor = new WaterVaporProcessor();
     processor.process(granule);
   }
 
   @Test(expected = NotEnoughImages.class)
-  public void cannotProcessMissingB09Granule() throws NotEnoughImages {
+  public void cannotProcessMissingB09Granule() throws NotEnoughImages, IOException {
     Granule granule = Granule.fromfiles(Arrays.asList(new File("T33UUP_20180804T100031_B01.tif")));
     WaterVaporProcessor processor = new WaterVaporProcessor();
     processor.process(granule);
